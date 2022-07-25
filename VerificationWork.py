4e5f8c4c1054ff56192abe3d67abee852531d4e1
AlexeyN <aleksgavr@gmail.com>
2022-07-25 23:25:39 +1200

    Added functions to clear the screen and handle user input errors

--- VerificationWork.py
+++ VerificationWork.py
@@ -3,26 +3,37 @@ import string
 from random import randint
 
 
+def cls():
+    print("/n" * 100)
+
+
+def get_quantity_rows():
+    while True:
+        try:
+            num = int(input("Введите количество строк из которых будет состоять массив: "))
+            return num
+        except ValueError:
+            print("Вы ввели нецелочисленное значение. Повторите ввод")
+
+
 def generate_alphanum_random_string(length):
     letters_and_digits = string.ascii_letters + string.digits
     rand_string = ''.join(random.sample(letters_and_digits, length))
-    s = rand_string
-    return s
+    return rand_string
 
 
-m = 0
-m = int(input("Введите количество строк из которых будет состоять массив: "))
-i = 0
+m = get_quantity_rows()
+
 Array = []
-
+NewArray = []
+lim = 3
+i = 0
 while i < m:
     n = randint(2, 5)
     Array.append(generate_alphanum_random_string(n))
     i += 1
 print(f'Случайный массив: {Array}')
 
-NewArray = []
-lim = 3
 i = 0
 while i < m:
     if len(Array[i]) <= lim:
